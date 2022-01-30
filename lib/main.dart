f24f596e4d804b048c797fecd7dbdf161500312a
Sam-2019 <samtmartey@gmail.com>
2022-01-30 21:53:37 +0000

    added floating button

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,38 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
+  runApp(const MaterialApp(home: Home()));
+}
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       appBar: AppBar(
-        title: Text('Planet'),
+        title: const Text('Planet'),
         centerTitle: true,
+        backgroundColor: Colors.grey[850],
       ),
       body: Center(
-        child: Text('Hello Planets'),
+        child: Text(
+          'Hello Planet',
+          style: TextStyle(
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 2.0,
+              color: Colors.grey[600],
+              fontFamily: 'IndieFlower'),
+        ),
       ),
-    ),
-  ));
+      floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.grey[600],
+          onPressed: () {},
+          child: const Icon(
+            Icons.add,
+            color: Colors.white,
+          )),
+    );
+  }
 }
